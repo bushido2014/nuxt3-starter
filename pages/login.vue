175fc2daa8b3abfca9e557fbfb1917993658f3a3
bushido2014 <joomjoom2011@gmail.com>
2024-06-25 12:55:07 +0300

    change form

--- pages/login.vue
+++ pages/login.vue
@@ -58,38 +58,31 @@ onMounted(() => {
     <div v-if="errorMsg" class="mb-10 p-4 rounded-md bg-light-grey shadow-lg">
       <p class="text-red-500">{{ errorMsg }}</p>
     </div>
-    <form
+    <UForm
       @submit.prevent="() => (isSignUp ? signUp() : login())"
       class="flex flex-col gap-2 mt-16"
     >
-      <input
-        type="email"
-        placeholder="Email"
-        required
-        v-model="email"
-        class="p-3 text-black rounded mb-2"
-      />
-      <input
-        type="password"
-        required
-        placeholder="Password"
-        v-model="password"
-        class="p-3 text-black rounded bg-gray-200 mb-2"
-      />
-      <button
+      <UFormGroup label="Email" name="email" size="xl">
+      <UInput v-model="email" placeholder="you@example.com" icon="i-heroicons-envelope"/>
+    </UFormGroup>
+     
+      <UFormGroup label="Password" name="password" size="xl">
+      <UInput v-model="password" type="password" placeholder="Password" />
+    </UFormGroup>
+      <UButton block size="xl>
         type="submit"
-        class="p-4 font-medium text-white bg-sky-500 rounded hover:bg-sky-700"
       >
         <span v-if="isSignUp"> Sign up </span>
         <span v-else> Log in </span>
-      </button>
-    </form>
-    <button
+      </UButton>
+    </UForm>
+    <UButton
       @click="isSignUp = !isSignUp"
-      class="w-full mt-8 text-sm text-center underline text-slate-600"
+      size="xl 
+      block
     >
       <span v-if="isSignUp"> Have an account? Log in instead </span>
       <span v-else> Create a new account </span>
-    </button>
+    </UButton>
   </div>
 </template>
